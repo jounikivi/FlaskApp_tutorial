47273921efc19f67705f7484bb6923eaa3d32339
Jouni Kiviperä <jounikivipera@gmail.com>
2022-01-17 09:51:25 +0200

    changes

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from . import db
-from flask import Blueprint, render_template, request, flash
+import json
+from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 from .models import Note
 
@@ -17,6 +18,18 @@ def home():
       new_note = Note(data=note, user_id=current_user.id)
       db.session.add(new_note)
       db.session.commit()
-      flash('Note added!')
+      flash('Note added!', category='success')
 
   return render_template('home.html', user=current_user)
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
